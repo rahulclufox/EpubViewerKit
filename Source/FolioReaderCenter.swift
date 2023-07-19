d624edcc7b91ed5e1e9c70f211c9bcd8390b3d57
ajithtom <ajithtomjacob607@gmail.com>
2023-07-19 14:39:11 +0530

    Add custom class only if not available

--- Source/FolioReaderCenter.swift
+++ Source/FolioReaderCenter.swift
@@ -479,25 +479,16 @@ open class FolioReaderCenter: UIViewController, UICollectionViewDelegate, UIColl
 
         let mediaOverlayStyleColors = "\"\(self.readerConfig.mediaOverlayColor.hexString(false))\", \"\(self.readerConfig.mediaOverlayColor.highlightColor().hexString(false))\""
 
-        var webHeight = UIScreen.main.bounds.height
-        if #available(iOS 13.0, *) {
-            let window = UIApplication.shared.windows.first
-            let topPadding = window?.safeAreaInsets.top ?? 0
-            let bottomPadding = window?.safeAreaInsets.bottom ?? 0
-            webHeight = webHeight - topPadding - bottomPadding
-        } else if #available(iOS 11.0, *) {
-            let window = UIApplication.shared.keyWindow
-            let topPadding = window?.safeAreaInsets.top ?? 0
-            let bottomPadding = window?.safeAreaInsets.bottom ?? 0
-            webHeight = webHeight - topPadding - bottomPadding
-        }
+        let webHeight = UIScreen.main.bounds.height
+    
         // Inject CSS
         let jsFilePath = Bundle.frameworkBundle().path(forResource: "Bridge", ofType: "js")
         let cssFilePath = Bundle.frameworkBundle().path(forResource: "PaginateStyle", ofType: "css")
         let cssTag = "<link rel=\"stylesheet\" type=\"text/css\" href=\"\(cssFilePath!)\" />"
         let jsTag = "<script type=\"text/javascript\" src=\"\(jsFilePath!)\"></script>" +
         "<script type=\"text/javascript\">setMediaOverlayStyleColors(\(mediaOverlayStyleColors))</script>"
-        let metaTag = "<meta name='viewport' content='width=device-width,height=\(webHeight),initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no' />"
+        
+        let metaTag = "<meta name=\"viewport\" content=\"width=device-width,height=\(webHeight),initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no\" />"
         
         let toInject = "\n\(metaTag)\n\(cssTag)\n\(jsTag)\n</head>"
         html = html.replacingOccurrences(of: "</head>", with: toInject)
@@ -513,8 +504,9 @@ open class FolioReaderCenter: UIViewController, UICollectionViewDelegate, UIColl
 
         // Font Size
         classes += " \(folioReader.currentFontSize.cssIdentifier)"
-
-        html = html.replacingOccurrences(of: "<html ", with: "<html class=\"\(classes)\" ")
+        if !html.contains(classes) {
+            html = html.replacingOccurrences(of: "<html ", with: "<html class=\"\(classes)\" ")
+        }
 
         // Let the delegate adjust the html string
         if let modifiedHtmlContent = self.delegate?.htmlContentForPage?(cell, htmlContent: html) {
