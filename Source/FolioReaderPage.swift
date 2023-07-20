0e9beb36d3d41a43f80494aa008dab466fd729a5
ajithtom <ajithtomjacob607@gmail.com>
2023-07-20 12:53:40 +0530

    Removed top padding of webview

--- Source/FolioReaderPage.swift
+++ Source/FolioReaderPage.swift
@@ -131,7 +131,7 @@ open class FolioReaderPage: UICollectionViewCell, WKNavigationDelegate, UIGestur
         let statusbarHeight = UIApplication.shared.statusBarFrame.size.height
         let navBarHeight = self.folioReader.readerCenter?.navigationController?.navigationBar.frame.size.height ?? CGFloat(0)
         let navTotal = self.readerConfig.shouldHideNavigationOnTap ? 0 : statusbarHeight + navBarHeight
-        let paddingTop: CGFloat = 20
+        let paddingTop: CGFloat = 0//20
         let paddingBottom: CGFloat = 50
 
         return CGRect(
